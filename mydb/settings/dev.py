1b7c89fae887fd5dfcd49c59122a20ebfebf2c0b
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-04 03:30:54 +0300

    New updates for Heroku deployment

---
+++ mydb/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y%b04g=7-7u++ahjf9)+q+rr8$8&u+(izh%9^x=z37h9#gsivd'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ readme.md
@@ -0,0 +1,4 @@
+git status
+git add -A
+git commit -m "New updates for Heroku deployment"
+git push -u origin master
